7a158a0430fe1628c66e43b91a1eb1e75efd059b
jinyalin <jinyalin@baidu.com>
2017-05-14 11:49:50 +0800

    first blood

--- src/test/java/com/yalin/stylepaser/test/ParserTest.java
+++ src/test/java/com/yalin/stylepaser/test/ParserTest.java
@@ -1,6 +1,7 @@
 package com.yalin.stylepaser.test;
 
 import com.yalin.styleparser.ResponseParser;
+import com.yalin.styleparser.StyleParser;
 import org.junit.Test;
 
 import java.io.BufferedReader;
@@ -25,5 +26,19 @@ public class ParserTest {
         }
         in.close();
         ResponseParser.parseResponse(sb.toString());
+    }
+
+    @Test
+    public void testDemos() throws Exception{
+        String[] demo1 = new String[]{"https://www.wikiart.org/en/john-miller/coconut-beach"};
+        String[] demo2 = new String[]{"https://www.wikiart.org/en/agnes-lawrence-pelton/untitled-1931"};
+        String[] demo3 = new String[]{"https://www.wikiart.org/en/agnes-lawrence-pelton/even-song-1930"};
+        String[] demo4 = new String[]{"https://www.wikiart.org/en/agnes-lawrence-pelton/fire-sounds-1930"};
+        String[] demo5 = new String[]{"https://www.wikiart.org/en/agnes-lawrence-pelton/spring-moon-1942"};
+        StyleParser.main(demo1);
+        StyleParser.main(demo2);
+        StyleParser.main(demo3);
+        StyleParser.main(demo4);
+        StyleParser.main(demo5);
     }
 }
